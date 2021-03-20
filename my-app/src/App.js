62c15561230eca0c61c66b44a9948f4f334c0397
rishank-shah <shahrishank@gmail.com>
2021-03-20 11:36:48 +0530

    navbar added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import { Route,Switch } from 'react-router';
 import Home from './components/Home'
+import SetTimer from './components/Timer/SetTimer'
+import NavBar from './components/NavBar'
 
 const App = () =>(
   <>
+    <NavBar/>
     <Switch>
       <Route exact path='/' component={Home} />
+      <Route exact path='/set-timer' component={SetTimer} />
     </Switch>
   </>
 )
---
+++ my-app/src/components/Timer/SetTimer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () =>(
+    <div>
+        <div className="jumbotron">
+            <h2>Home</h2>
+            <p className="lead">Welcome to Home page</p>
+        </div>
+    </div>
+);
+
+export default Home;
